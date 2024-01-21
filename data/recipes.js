48120e9651e4d08b03be0a87ce800419879e3622
Alien2080 <tom_aylen@hotmail.com>
2024-01-21 12:04:53 +1030

    change styling, add recipe steps for 3 recipes

--- data/recipes.js
+++ data/recipes.js
@@ -1,3 +1,8 @@
 // placeholder in case I need it.
 // TODO: Add rendomise button to select a recipe
 // TODO: Add named timer/s (if useful) 
+
+// Basic script for any dynamic functionality you might add later
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Document is ready!');
+});
